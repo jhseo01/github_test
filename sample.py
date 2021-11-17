e42df4603233795bf954f2fcbc05262a6627c7c1
jhseo01 <anonymhk@gmail.com>
2021-11-18 00:22:21 +0900

    tray_icon_test

---
+++ sample.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+def getHello():
+    return "Hello"
+
+def sum(a,b):
+    return a+b
+
+import numpy as np
+
+def numpy_test():
+    a = np.random.randint(6)
+    return a
+
+print(numpy_test())
